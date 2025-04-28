cb0295735af35c6bca49e0cfd056a1f184417d09
lily.khadarina <litater@rambler.ru>
2025-04-28 22:37:01 +0300

    Отладка hit-null x4

--- stats/stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ stats/stats-server/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -3,6 +3,7 @@ package ru.practicum.exception;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -18,25 +19,32 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequestException(final StatsBadRequestException e) {
         log.warn("Ошибка запроса: {}", e.getMessage());
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        String stackTrace = sw.toString();
+        String stackTrace = printStackTrace(e);
         return new ErrorResponse("Проверьте соотвествие передаваемых данных требуемым", e.getMessage(), stackTrace);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(MethodArgumentNotValidException e) {
+        log.warn("Ошибка валидации аргументов метода: {}", e.getMessage());
+        String stackTrace = printStackTrace(e);
+        return new ErrorResponse("Некорректные данные", e.getMessage(), stackTrace);
+    }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleInternalServerException(final Exception e) {
         log.warn("Непредвиденная ошибка: {}", e.getMessage());
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
-        e.printStackTrace(pw);
-        String stackTrace = sw.toString();
+        String stackTrace = printStackTrace(e);
         return new ErrorResponse("Сервис статистики временно недоступен", e.getMessage(), stackTrace);
     }
 
+    private String printStackTrace(Exception e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        return sw.toString();
+    }
 
     private record ErrorResponse(String error, String description, String stackTrace) {
     }
