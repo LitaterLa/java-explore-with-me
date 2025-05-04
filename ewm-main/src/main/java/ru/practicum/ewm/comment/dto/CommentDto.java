06e218969e5ecbe35449c67b635bb709720fb770
lily.khadarina <litater@rambler.ru>
2025-05-04 21:37:51 +0300

    Фикс названия ветки

--- ewm-main/src/main/java/ru/practicum/ewm/comment/dto/CommentDto.java
+++ ewm-main/src/main/java/ru/practicum/ewm/comment/dto/CommentDto.java
@@ -4,15 +4,17 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 import ru.practicum.ewm.event.dto.EventDto;
 import ru.practicum.ewm.user.dto.UserDto;
 
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
--- ewm-main/src/main/java/ru/practicum/ewm/comment/dto/NewCommentDto.java
+++ ewm-main/src/main/java/ru/practicum/ewm/comment/dto/NewCommentDto.java
@@ -5,11 +5,13 @@ import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.FieldDefaults;
 
-@Data
+@Getter
+@Setter
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @AllArgsConstructor
